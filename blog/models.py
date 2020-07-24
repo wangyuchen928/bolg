7ef0ddfbe3a632fb79dc5180ead0f0741d51bc39
所爱隔山海 <941826670@qq.com>
2020-07-24 09:48:51 +0800

    修改版本及小部分代码（代码正常运行）

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     # 用于url 作为一个简短的标记
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     # 一个外键 一个用户可以拥有多个帖子 （多对一）
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_query_name="blog_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     # 贴子的主体
     body = models.TextField()
     # 发布帖子的日期
