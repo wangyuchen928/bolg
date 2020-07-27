ac38c2f7cccb1d528980fca1dda6d29bc4c15c8d
所爱隔山海 <941826670@qq.com>
2020-07-27 18:28:29 +0800

    修改模型类

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='publish')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', "Published"),
+        ('draft', '隐藏'),
+        ('published', "显示"),
     )
     # 帖子的标题
     title = models.CharField(max_length=250)
@@ -30,3 +36,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
