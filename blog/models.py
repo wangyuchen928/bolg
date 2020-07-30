f3a8f32a5541a8bfaf3906c4a6c6625bcb6c6d2e
所爱隔山海 <941826670@qq.com>
2020-07-30 08:33:34 +0800

    创建评论数据模型

--- blog/models.py
+++ blog/models.py
@@ -42,3 +42,26 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    # 名字
+    name = models.CharField(max_length=80)
+    # 邮箱
+    email = models.EmailField()
+    # 评论内容
+    body = models.TextField()
+    # 创建时间
+    created = models.DateTimeField(auto_now_add=True)
+    # 修改时间
+    updated = models.DateTimeField(auto_now=True)
+    # 手动禁用某些不恰当的评论
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by %s on %s' % (self.name, self.post)
